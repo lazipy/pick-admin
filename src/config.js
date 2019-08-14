b479466ca354347bb8aa18221ae8f3fa2559b43e
李子屏 <877286986@qq.com>
2019-08-14 15:58:10 +0800

    style(element): element - UI theme
    Customize the element-ui theme

--- src/config.js
+++ src/config.js
@@ -1,21 +1,39 @@
 export default {
   routes: [
     {
-      indexRouter: true,
-      path: '/',
-      name: 'welcome',
-      component: () => import('@/pages/Welcome.vue'),
-      meta: {
-        title: '欢迎使用'
-      }
-    },
-    {
       path: '/login',
       name: 'login',
       component: () => import('@/pages/Login.vue'),
       meta: {
         title: '登录'
       }
-    }
+    },
+    {
+      indexRouter: true,
+      path: '',
+      component: () => import('@/pages/Layout.vue'),
+      redirect: 'welcome',
+      children: [
+        {
+          path: 'welcome',
+          name: 'welcome',
+          component: () => import('@/pages/Welcome.vue'),
+          meta: { title: '首页' }
+        }
+      ]
+    },
+    {
+      path: '/404',
+      name: 'notFount',
+      component: () => import('@/pages/404.vue'),
+      meta: { title: '404' }
+    },
+    {
+      path: '/500',
+      name: 'error',
+      component: () => import('@/pages/500.vue'),
+      meta: { title: '500' }
+    },
+    { path: '*', redirect: '/404' }
   ]
 }
