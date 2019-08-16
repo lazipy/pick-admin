48446a729b8a87fd95df56e9f3169147b5257806
李子屏 <877286986@qq.com>
2019-08-16 15:01:44 +0800

    feat(error page): add error pages
    add 404 page and 500 page

---
+++ src/pages/404.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="not-fount">
+    <img className="image" src="../assets/images/404.png" alt="404" >
+    <div className="descript">
+      <p className="intro">抱歉，您访问的页面不存在...</p>
+      <el-button type="primary" @click="() => $router.replace('/')">返回首页</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFount'
+}
+</script>
+
+<style lang="scss" scoped>
+  .not-fount {
+    width: 900px;
+    margin: 80px auto 0;
+    text-align: center;
+
+    .descript {
+      margin-top: 20px;
+    }
+    .intro {
+      padding: 40px 0 20px 0;
+      margin: 0;
+      font-size: 20px;
+    }
+  }
+</style>
---
+++ src/pages/500.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="error">
+    <img className="image" src="../assets/images/500.png" alt="500" >
+    <div className="descript">
+      <p className="intro">抱歉，您访问的页面出错了...</p>
+      <el-button type="primary" @click="() => $router.replace('/')">返回首页</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error'
+}
+</script>
+
+<style lang="scss" scoped>
+  .error {
+    width: 900px;
+    margin: 80px auto 0;
+    text-align: center;
+
+    .descript {
+      margin-top: 20px;
+    }
+    .intro {
+      padding: 40px 0 20px 0;
+      margin: 0;
+      font-size: 20px;
+    }
+  }
+</style>
