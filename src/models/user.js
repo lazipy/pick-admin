35b19288b17c97d14e21fec37b90777c56a888fd
李子屏 <877286986@qq.com>
2019-08-22 11:25:09 +0800

    feat(icon): change icon
    change ico

--- src/models/user.js
+++ src/models/user.js
@@ -1,38 +1,5 @@
 import { userLogin, queryUserInfo, queryUserMenus } from '@/services/user'
-
-/**
- * 通过route name判断是否与当前用户权限匹配
- * @param permissionsMap
- * @param route
- */
-function hasPermission (permissionsMap, route) {
-  for (let map of permissionsMap) {
-    if (map.name === route.name) {
-      return true
-    } else if (map.children && map.children.length) {
-      return hasPermission(map.children, route)
-    }
-  }
-  return false
-}
-
-/**
- * 递归过滤异步路由表，返回符合用户角色权限的路由表
- * @param asyncRouterMap
- * @param permissionsMap
- */
-function filterAsyncRouter (asyncRouterMap, permissionsMap) {
-  const accessedRouters = asyncRouterMap.filter(route => {
-    if (hasPermission(permissionsMap, route)) {
-      if (route.children && route.children.length) {
-        route.children = filterAsyncRouter(route.children, permissionsMap)
-      }
-      return true
-    }
-    return false
-  })
-  return accessedRouters
-}
+import { filterAsyncRouter } from '@/shared/utils/auth'
 
 export default {
   namespaced: true,
